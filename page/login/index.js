0f0d31111abc244994a8b92eb772317db5c70014
xnylh <xnylh@xnylh.cn>
2024-04-17 12:14:09 +0800

    登录bug-验证码长度判断

--- page/login/index.js
+++ page/login/index.js
@@ -15,6 +15,10 @@ document.querySelector('.btn').addEventListener('click', () => {
     myAlert(false,'手机号长度要为11位')
     return
   }
+  if(data.code.length !==11){
+    myAlert(false,'验证码长度要为6位')
+    return
+  }
   // 1.3 基于 axios 调用验证码登录接口
   axios({
     url: '/v1_0/authorizations',
